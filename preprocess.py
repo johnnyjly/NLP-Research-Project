584ca3354c6fb3e0b6bf1e6568457775dc00b0ad
Shaohong Chen <shaohong.chen@mail.utoronto.ca>
2024-04-14 20:57:55 -0400

    update preprocess

--- preprocess.py
+++ preprocess.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import re
 
 '''
 Some Data preprocessing and Data Visual Analysis
@@ -34,55 +35,6 @@ Columns of dataset:
          # then add lower to input and predict upper
 
 '''
-
-def preprocess_data_deprecated(data_path):
-
-    # Step 0: Load data
-    data = pd.read_csv(data_path)
-    data = data.dropna().drop(columns=['Rating', 'Revenue', 
-            'Competitors', 'Hourly', 'Employer provided', 'Company Name', 
-            'Job Location', 'job_title_sim', 'seniority_by_title',  
-            'Salary Estimate', 'Age', 'Avg Salary(K)'])
-
-    # Step 1: Concatenate the string parts
-    data['string'] = data['Job Title'] + '-' +\
-        data['Job Description'] + '-' +\
-        data['Location'] + '-' +\
-        data['Headquarters'] + '-' +\
-        data['company_txt'] + '-' +\
-        str(data['Founded'])
-    
-    # Step 2: Combine Bool columns into one number, converted from binary
-    skill_list = ['Python', 'spark', 'aws', 'excel', 'sql', 'sas', 'keras',
-                   'pytorch', 'scikit', 'tensor', 'hadoop', 'tableau', 'bi',
-                   'flink', 'mongo', 'google_an']
-    # Note: The order of skills is inverted to digits of binary number
-    #       e.g. 0b0000000000000001 implies only requiring Python
-    #            0b1000000000000000 implies only requiring google_an
-    data['skills'] = 0
-    for skill in skill_list:
-        data['skills'] += data[skill] * (2 ** (skill_list.index(skill)))
-    
-    # Step 3: Convert categorical columns into numerical values
-    category_list = ['Size', 'Type of ownership', 'Industry', 
-                          'Sector', 'Degree']
-    category_ref = {}
-    for category in category_list:
-        data[category] = data[category].astype('category')
-        category_ref[category] = data[category].cat.categories
-        data[category] = data[category].cat.codes
-
-    # Step 4: Create target columns
-    data['target_l'] = data['Lower Salary'] 
-    data['target_u'] = data['Upper Salary']
-
-    # Step 5: Select Processed Outputs
-    output_cols = ['string', 'skills', 'Size', 'Type of ownership', 'Industry', 
-                   'Sector', 'Degree', 'target_l', 'target_u']
-    # output_cols = ['string', 'target_l']
-    data[output_cols].to_csv('./data/preprocessed.csv', index=False)
-    return (skill_list, category_list, data[output_cols])
-
 
 def preprocess_data(data_path):
 
@@ -124,6 +76,8 @@ def preprocess_data(data_path):
         data['category_str'] = data.apply(lambda x: '{}{}: {};'.format(x.category_str, category, x[category]), axis=1)
     data['string'] = data['string'] + data['skill_str'] + data['category_str']
 
+    data['string'] = data.apply(lambda x: pre_process_text(x['string']), axis=1)
+    
     # Step 4: Create target columns
     data.rename(columns={'Lower Salary': 'target_l', 'Upper Salary': 'target_u'}, inplace=True)
 
@@ -132,6 +86,14 @@ def preprocess_data(data_path):
     # output_cols = ['string', 'target_l']
     data[output_cols].to_csv('./data/preprocessed.csv', index=False)
     return data[output_cols]
+
+
+def pre_process_text(text):
+  text = re.sub("[^a-zA-Z]", " ", text)
+  text = text.lower()
+  tokens = text.split()
+  return " ".join(tokens)
+
 
 if __name__ == '__main__':
     preprocess_data('./data/data_cleaned_2021.csv')
